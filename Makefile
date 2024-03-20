bc534b9bd0bbed83b207994b04e6682f98f20db4
vehe538 <juro.mesik@gmail.com>
2024-03-21 00:47:51 +0100

    ..

---
+++ Makefile
@@ -0,0 +1,8 @@
+CC=gcc
+
+
+eratosthenes: eratosthenes.c
+	$(CC) eratosthenes.c -o eratosthenes -lm
+
+
+
---
+++ eratosthenes.c
@@ -0,0 +1,39 @@
+//#include "bitset.h"
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+
+    int n = 10000;
+    int m = 0;
+    int all[n];
+    int primes[n];
+
+    int c = 0;
+    for (int i = 2; i <= n; i++){
+        all[c] = i;
+        c++;
+    }
+
+    c = 0;
+    for (int j = 2; j <= n; j++){
+        for (int c = 0; c < n; c++){
+            if (all[c] != j && all[c] % j == 0 && all[c] != 0){
+                all[c] = 0;
+
+            }
+        }
+        
+    }
+
+    for (int i = 0; i < n; i++){
+        if (all[i] != 0){
+            primes[m] = all[i];
+            printf("%d, ", primes[m]);
+            m++;
+        }
+    }
+
+    return 0;
+}
+
